acb6972073853b570c7b9e5a384b58bfe821146e
MaratFront <alievmarat3988@gmail.com>
2025-03-12 14:57:20 +0200

    first-server

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/hello', (req, res) => {
+    res.send('Привіт, світ!');
+});
+
+app.get('/json', (req, res) => {
+    res.json({ "message": "Це JSON-відповідь" });
+});
+
+app.get('/user/:id', (req, res) => {
+    const id = req.params.id;
+    res.send("Користувач з ID: ${id})");
+});
+
+app.get('/search', (req, res) => {
+    const query = req.query.q;
+    res.send(`Пошук за запитом: ${query}`);
+});
+
+app.listen(PORT, () => {
+    console.log(`Сервер запущено на порту ${PORT}`);
+});
+
+app.listen(3000, () => {
+    console.log('Сервер працює на http://localhost:3000');
+});
