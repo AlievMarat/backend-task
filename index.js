c2e78fa810d59bd73600067707e5baecb519be92
AlievMarat <115290688+AlievMarat@users.noreply.github.com>
2025-03-16 19:38:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,3 @@ app.get('/search', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Сервер запущено на порту ${PORT}`);
 });
-
-app.listen(3000, () => {
-    console.log('Сервер працює на http://localhost:3000');
-});
